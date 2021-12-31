93b3ff1cdc11c80fc9aa862ebc576dd02c5c3b45
Hanabira <workflow.elec@gmail.com>
2021-12-31 19:40:54 +0300

    feat/add sorts dir for sorts algo

---
+++ python-way/algorithms/sorts/quicksort.py
@@ -0,0 +1,16 @@
+# TODO: create me!
+def sort(lst, low, high):
+    '''
+    link: https://en.wikipedia.org/wiki/Quicksort
+    
+    speed (average): O(n*log(n))
+    '''
+    if len(lst) == 1:
+        return lst
+    
+    
+
+
+
+if __name__ == 'main':
+    pass
