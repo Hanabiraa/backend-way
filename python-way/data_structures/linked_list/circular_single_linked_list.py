96b5f153649c3cd1e7f287c1df8c4d0ace974fc3
Hanabira <workflow.elec@gmail.com>
2022-02-17 14:35:50 +0300

    feat/add CLL implementation

---
+++ python-way/data_structures/linked_list/circular_single_linked_list.py
@@ -0,0 +1,65 @@
+from typing import Any
+
+
+class _Node():
+    """
+    Node builder for circular single linked list implementation
+    """
+
+    def __init__(self, data: Any) -> None:
+        self.data = data
+        self.next = None
+
+
+class CircularSingleLinkedList():
+    def __init__(self) -> None:
+        self.last = None
+
+    def append(self, data: Any) -> None:
+        new_node = _Node(data)
+        if not self.last:
+            self.last = new_node
+            new_node.next = self.last
+        new_node.next = self.last.next
+        self.last.next = new_node
+
+    def remove(self) -> Any:
+        if not self.last:
+            return
+
+        del_node = self.last
+        
+        if self.last == self.last.next:
+            self.last = None
+            return del_node.data
+
+        tmp = self.last.next
+        while tmp.next != self.last:
+            tmp = tmp.next
+        self.last = self.last.next
+        tmp.next = self.last
+        return del_node.data
+
+    def __str__(self) -> str:
+        if not self.last:
+            return '[]'
+
+        res = ""
+        template = "[{}] -> "
+        tmp = self.last
+        while tmp.next != self.last:
+            res += template.format(tmp.data)
+            tmp = tmp.next
+        else:
+            res += template.format(tmp.data)
+        return res
+
+
+if __name__ == "__main__":
+    cll = CircularSingleLinkedList()
+    for data in range(10):
+        cll.append(data)
+    print(cll)
+    for _ in range(5):
+        print(f'del: {cll.remove()}')
+    print(cll)
