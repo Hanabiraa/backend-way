75abbdb3f80695a008934497b74103693d16e470
Hanabira <workflow.elec@gmail.com>
2022-02-27 03:44:02 +0300

    feat/add basic binary tree operations

--- python-way/data_structures/binary_tree/binary_tree.py
+++ python-way/data_structures/binary_tree/binary_tree.py
@@ -138,32 +138,60 @@ def bin_tree_is_perfect(root: Node) -> bool:
 
     return check(root, max_depth, curr_depth)
 
+
 def bin_tree_is_complete(root: Node) -> bool:
     """
     checks if the tree is complete
 
     Use index property.
-    
+    If the index of any element in the array is `i`, the element in the index `2i+1` will become
+    the left child and element in `2i+2` index will become the right child.
+    Also, the parent of any element at index `i` is given by the lower bound of `(i-1)/2`.
     """
-    max_depth = curr_depth = 0
-    tmp_root = root
-    while tmp_root.left:
-        max_depth += 1
-        tmp_root = tmp_root.left
+    def count_nodes(root: Node) -> int:
+        """
+        count all nodes in tree
+        """
+        if root is None:
+            return 0
+        return 1 + count_nodes(root.left) + count_nodes(root.right)
 
-    def check(root, max_depth: int, curr_depth: int) -> bool:
+    def check(root: Node, index: int, max_node: int) -> bool:
+        if root is None:
+            return True
+        if index >= max_node:
+            return False
+        return check(root.left, 2*index+1, max_node) and check(root.right, 2*index+2, max_node)
+    return check(root, 0, count_nodes(root))
+
+
+def bin_tree_is_balanced(root: Node) -> bool:
+    """
+    checks if the tree is balance
+
+    An empty tree is height-balanced. A non-empty binary tree T is balanced if: 
+    1) Left subtree of T is balanced 
+    2) Right subtree of T is balanced 
+    3) The difference between heights of left subtree and right subtree is not more than 1. 
+    """
+    def height(root: Node) -> int:
+        """
+        count height for curr sub-tree
+        """
+        if root is None:
+            return 0
+        return 1 + max(height(root.left), height(root.right))
+
+    def check(root: Node) -> bool:
         if root is None:
             return True
 
-        if root.left is None and root.right is None:
-            return max_depth == curr_depth
-
-        if root.left is None or root.right is None:
-            return False
-
-        return check(root.left, max_depth, curr_depth+1) and check(root.right, max_depth, curr_depth+1)
-
-    return check(root, max_depth, curr_depth)
+        l_height = height(root.left)
+        r_height = height(root.right)
+        if 0 <= abs(l_height - r_height) <= 1 and check(root.left) and check(root.right):
+            return True
+        return False
+    return check(root)
 
 def main():
     """ 
@@ -203,6 +231,30 @@ def main():
     print("Is this tree perfect?: ", bin_tree_is_perfect(tree3))
     print("Is this tree perfect?: ", bin_tree_is_perfect(tree2))
 
+    """ 
+    Constructed binary tree is
+            1
+          /   \
+         2     3
+              / \
+             8   9
+    """
+    tree4 = Node(1, Node(2), Node(3, Node(8), Node(9)))
+    print("Is this tree complete?: ", bin_tree_is_complete(tree1))
+    print("Is this tree complete?: ", bin_tree_is_complete(tree4))
 
+    """ 
+    Constructed binary tree is
+            1
+          /   \
+         2     3
+              / \
+             8   9
+            /
+           3
+    """
+    tree5 = Node(1, Node(2), Node(3, Node(8, Node(3)), Node(9)))
+    print("Is this tree height-balanced?: ", bin_tree_is_balanced(tree1))
+    print("Is this tree height-balanced?: ", bin_tree_is_balanced(tree5))
 if __name__ == "__main__":
     main()
