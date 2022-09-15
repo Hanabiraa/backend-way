018e3ab17281bdeac7dce3f7645ee766006c3647
Hanabira <workflow.elec@gmail.com>
2022-09-15 14:14:30 +0300

    feat/add chapter 4 app tests

--- django-way/django-for-beginners/chapter4-message-board/posts/tests.py
+++ django-way/django-for-beginners/chapter4-message-board/posts/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.post = Post.objects.create(text='This is a test!')
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, 'This is a test!')
+
+    def test_url_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_url_location_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_right_template_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertTemplateUsed(resp, 'home.html')
+
+    def test_template_content(self):
+        resp = self.client.get(reverse('home'))
+        self.assertContains(resp, 'This is a test!')
+
+    def test_homepage(self):
+        """
+        пример объединения несколько юниттестов, т.к. общий смысл
+        """
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+        self.assertContains(resp, 'This is a test!')
