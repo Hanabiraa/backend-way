113f75ac35bf281c161eb0ae3e8bb5589bc6e794
Hanabira <workflow.elec@gmail.com>
2022-09-25 21:44:56 +0300

    feat/add tests for custom user model

--- django-way/django-for-beginners/6_news/accounts/tests.py
+++ django-way/django-for-beginners/6_news/accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignupPageTests(TestCase):
+    def test_url_exists_at_correct_loc(self):
+        resp = self.client.get('/accounts/signup/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_signup_view_name(self):
+        resp = self.client.get(reverse('signup'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed('registration/signup.html')
+
+    def test_signup_form(self):
+        resp = self.client.post(
+            reverse('signup'),
+            {
+                'username': 'testuser',
+                'email': 'testuser@email.com',
+                'password1': 'testpass123',
+                'password2': 'testpass123',
+            },
+        )
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'testuser')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'testuser@email.com')
+
