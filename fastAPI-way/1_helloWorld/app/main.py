18540d38756880c37dff9a2995ad68ccd6170b72
Hanabira <workflow.elec@gmail.com>
2022-11-03 19:06:30 +0300

    feat/add complete 2 chapter

--- fastAPI-way/1_helloWorld/app/main.py
+++ fastAPI-way/1_helloWorld/app/main.py
@@ -1,5 +1,26 @@
 from fastapi import FastAPI, APIRouter
 
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
+
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
@@ -11,6 +32,16 @@ def root() -> dict:
     Root GET
     """
     return {"msg": "Hello, World!"}
+
+
+@api_router.get("/recipe/{recipe_id}", status_code=200)
+def fetch_recipe(*, recipe_id: int) -> dict:
+    """
+    Fetch single recipe by ID
+    """
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result
 
 
 app.include_router(api_router)
