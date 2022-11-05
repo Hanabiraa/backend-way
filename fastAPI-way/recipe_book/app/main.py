6f81ec7826f4f03a1de98c6d77243f42f63dfa6b
Hanabira <workflow.elec@gmail.com>
2022-11-05 18:47:05 +0300

    feat/add complete 6 chapter

--- fastAPI-way/recipe_book/app/main.py
+++ fastAPI-way/recipe_book/app/main.py
@@ -1,21 +1,32 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
+from fastapi.templating import Jinja2Templates
 
-from typing import Optional
+from typing import Optional, Any
+from pathlib import Path
 
 from app.schemas import RecipeSearchResults, RecipeCreate, Recipe
 from app.recipe_data import RECIPES
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
 
+# Updated to serve a Jinja2 template
+# https://www.starlette.io/templates/
+# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
 @api_router.get("/", status_code=200)
-def root() -> dict:
+def root(request: Request) -> dict:
     """
     Root GET
     """
-    return {"msg": "Hello, World!"}
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": RECIPES}
+    )
 
 
 @api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
