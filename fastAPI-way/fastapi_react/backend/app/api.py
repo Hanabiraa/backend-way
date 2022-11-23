425cd7d90fb1c84e4d92933d7d0d4962e2e57eae
Hanabiraa <workflow.elec@gmail.com>
2022-11-23 15:26:48 +0300

    feat/add full project

--- fastAPI-way/fastapi_react/backend/app/api.py
+++ fastAPI-way/fastapi_react/backend/app/api.py
@@ -20,3 +20,59 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
+
+
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    }
+]
+
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return {"data": todos}
+
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: dict) -> dict:
+    response = {}
+    if todo not in todos:
+        todos.append(todo)
+        response["data"] = {"Todo added."}
+    else:
+        response["data"] = {"Todo duplicate!"}
+    return response
+
+
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["item"] = body["item"]
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
+
+
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todos.remove(todo)
+            return {
+                "data": f"Todo with id {id} has been removed."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
