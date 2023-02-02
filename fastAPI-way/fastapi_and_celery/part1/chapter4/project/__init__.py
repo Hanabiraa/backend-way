59ad48aec8090279752244af4580e2861cdeff9f
Vitaly Kopylov <workflow.elec@gmail.com>
2023-02-02 17:41:13 +0300

    feat/add chapter4 auto-reload celery worker and fastapi-web. Fix issue with watchfiles throw environment variable

---
+++ fastAPI-way/fastapi_and_celery/part1/chapter4/project/__init__.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from project.celery_utils import create_celery
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    # do it before load routes
+    app.celery_app = create_celery()
+
+    from project.users import user_router
+
+    app.include_router(user_router)
+
+    @app.get("/")
+    async def root():
+        return {"message": "Hello world"}
+
+    return app
